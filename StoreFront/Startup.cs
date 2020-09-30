31808f621e1171b71e172d5fa3d8c7fb190d9dad
Tyler Fierro <tmfierro@outlook.com>
2020-09-30 06:32:13 -0700

    commit1

---
+++ StoreFront/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreFront.Startup))]
+namespace StoreFront
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
